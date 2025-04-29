5753dbae9075a4fc7afccddf297581a4a4e9cdc5
Sergey <dzhigulev@yandex.ru>
2025-04-29 16:16:13 +0400

    Change methods for data transfer across create folder and dashboards

--- PythonTests/services/api_dashboards_service.py
+++ PythonTests/services/api_dashboards_service.py
@@ -2,6 +2,9 @@ import requests
 import PythonTests.config.settings as settings
 import PythonTests.data.dashboards_data as data
 
+from PythonTests.services.utils import write_value_in_json, read_value_in_json
+
+
 class ApiDashboardsService:
 
     @staticmethod
@@ -16,12 +19,19 @@ class ApiDashboardsService:
         assert response.status_code == 200, f'Expected status code 200, got {response.status_code}'
         assert response.json().get('title') == 'Folder for API Test'
 
-        return response.json().get('uid')
+        folder_uid = response.json().get('uid')
+
+        write_value_in_json('./data/dashboards.json', folder_uid, 'folderUid')
+
+        return folder_uid
 
     @staticmethod
-    def create_dashboard(folder_uid:str):
+    def create_dashboard():
         url = f'{settings.BASE_URL}/api/dashboards/db'
         headers = {'Content-Type': 'application/json'}
+
+        folder_uid = read_value_in_json('./data/dashboards.json', 'folderUid')
+
         body = data.get_body_for_create_dashboard(folder_uid)
         response = requests.post(url,
                                  auth = settings.BASIC_AUTH,
@@ -29,4 +39,7 @@ class ApiDashboardsService:
                                  headers=headers)
         assert response.status_code == 200, f'Expected status code 200, got {response.status_code}'
 
-        return response.json().get('uid')
+        dashboard_uid = response.json().get('uid')
+
+        write_value_in_json('./data/dashboards.json', dashboard_uid, 'dashboardUid')
+        return dashboard_uid
