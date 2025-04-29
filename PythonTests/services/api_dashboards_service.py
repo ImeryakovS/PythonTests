2dc29ad673c83b6fd87769dcd84bf9b0c7509209
Sergey <dzhigulev@yandex.ru>
2025-04-30 00:50:58 +0400

    add method for delete dashboard from folder

--- PythonTests/services/api_dashboards_service.py
+++ PythonTests/services/api_dashboards_service.py
@@ -1,6 +1,7 @@
 import requests
 import PythonTests.config.settings as settings
 import PythonTests.data.dashboards_data as data
+import logging
 
 from PythonTests.services.utils import write_value_in_json, read_value_in_json
 
@@ -43,3 +44,25 @@ class ApiDashboardsService:
 
         write_value_in_json('./data/dashboards.json', dashboard_uid, 'dashboardUid')
         return dashboard_uid
+
+    @staticmethod
+    def delete_dashboard():
+        dashboard_uid = read_value_in_json('./data/dashboards.json', 'dashboardUid')
+
+        url = f'{settings.BASE_URL}/api/dashboards/uid/{dashboard_uid}'
+
+        headers = {'Content-Type': 'application/json'}
+
+        body = data.get_body_for_create_dashboard('get_title')
+        title = body['dashboard']['title']
+
+        logging.info(f'title: {title}')
+        response = requests.delete(url,
+                                   auth = settings.BASIC_AUTH,
+                                   headers = headers)
+
+        assert response.status_code == 200, f'Expected status code 200, got {response.status_code}'
+        assert response.json().get('title') == title
+        assert response.json().get('message') == f'Dashboard {title} deleted'
+
+        return True
