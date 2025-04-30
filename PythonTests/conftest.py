3d45014ac54d2b235e1590f3d7fe5e301cda5c9a
Sergey <dzhigulev@yandex.ru>
2025-04-30 23:05:01 +0400

    add logging in fixtures

--- PythonTests/conftest.py
+++ PythonTests/conftest.py
@@ -1,5 +1,3 @@
-import atexit
-
 import pytest
 import shutil
 import os
@@ -13,19 +11,23 @@ from PythonTests.services.api_users_service import ApiUsersService
 def create_users_jsons():
     if not os.path.exists('./data/users.json'):
         shutil.copy('./data/users.template.json', './data/users.json')
+    logging.info("Creating users.json")
 
 @pytest.fixture(scope="session",autouse=True)
 def create_dashboards_jsons():
     if not os.path.exists('./data/dashboards.json'):
         shutil.copy('./data/dashboards.template.json', './data/dashboards.json')
+    logging.info("Creating dashboards.json")
 
 @pytest.fixture(scope="session",autouse=True)
 def create_low_access_user():
     ApiUsersService.create_api_user(low_access_credentials)
+    logging.info("Creating Low Access User")
 
 @pytest.fixture(scope="session",autouse=True)
 def create_existing_user():
     ApiUsersService.create_api_user(existing_credentials)
+    logging.info("Creating Existing User")
 
 def pytest_sessionfinish(session, exitstatus):
     delete_user_by_login(existing_credentials)
