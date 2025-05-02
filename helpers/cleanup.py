ba5c6cb46d48456480b356c6ad5a4e67856eed6a
Sergey <dzhigulev@yandex.ru>
2025-05-02 22:45:48 +0400

    Save only allure files

--- helpers/cleanup.py
+++
@@ -1,9 +0,0 @@
-import inspect
-import logging
-
-from services.api_users_service import ApiUsersService
-
-def delete_user_by_login(dictionary):
-    user_id = ApiUsersService.find_user_by_login(dictionary['login'])
-    ApiUsersService.delete_api_user(user_id)
-    logging.info(f'Function: {inspect.currentframe().f_code.co_name}, {dictionary["login"]} is deleted')
