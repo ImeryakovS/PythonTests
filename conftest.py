ba2a2f2eca1bfefdae9bb89266e6da70ae3e6f94
Sergey <dzhigulev@yandex.ru>
2025-05-02 19:49:20 +0400

    add allure in fixtures

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 import shutil
 import os
@@ -8,19 +9,23 @@ from data.users_credentials import existing_credentials, low_access_credentials
 from helpers.cleanup import delete_user_by_login
 from services.api_users_service import ApiUsersService
 
+
 @pytest.fixture(scope="session", autouse=True)
+@allure.title("Creating users.json from template")
 def create_users_jsons():
     if not os.path.exists(settings.USERS_PATH):
         shutil.copy('./data/users.template.json', settings.USERS_PATH)
     logging.info("Creating users.json")
 
 @pytest.fixture(scope="session",autouse=True)
+@allure.title("Creating dashboards.json from template")
 def create_dashboards_jsons():
     if not os.path.exists(settings.DASHBOARDS_PATH):
         shutil.copy('./data/dashboards.template.json', settings.DASHBOARDS_PATH)
     logging.info("Creating dashboards.json")
 
 @pytest.fixture(scope="session",autouse=True)
+@allure.title("Creating user with low access for tests")
 def create_low_access_user():
     try:
         response = ApiUsersService.create_api_user(low_access_credentials)
@@ -32,6 +37,7 @@ def create_low_access_user():
         pytest.exit(f"Critical error in fixture: stopping test execution")
 
 @pytest.fixture(scope="session",autouse=True)
+@allure.title("Creating existing user for tests")
 def create_existing_user():
     try:
         response = ApiUsersService.create_api_user(existing_credentials)
@@ -42,6 +48,7 @@ def create_existing_user():
         logging.critical(f"Exception during ExistingUser creation: {e}")
         pytest.exit(f"Critical error in fixture: stopping test execution")
 
+@allure.title("Delete users from previously steps accross pytest hook")
 def pytest_sessionfinish(session, exitstatus):
     delete_user_by_login(existing_credentials)
     delete_user_by_login(low_access_credentials)
