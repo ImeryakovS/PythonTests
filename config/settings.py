905e264c6e9c354f84a5608cabfacd5d23a90cd7
Sergey <dzhigulev@yandex.ru>
2025-05-02 20:31:14 +0400

    Try to fix Ci/cd actions (paths)

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+BASE_DIR = os.environ.get("GITHUB_WORKSPACE", os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
