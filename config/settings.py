9a844401021175047c8a46a8850ff8c5bd4bb1f4
Sergey <dzhigulev@yandex.ru>
2025-05-02 20:21:53 +0400

    Fix abs paths

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,14 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+USERS_PATH = os.path.join(DATA_DIR, 'users.json')
+DASHBOARDS_PATH = os.path.join(DATA_DIR, 'dashboards.json')
+USERS_TEMPLATE_PATH = os.path.join(DATA_DIR, 'users.template.json')
+DASHBOARDS_TEMPLATE_PATH = os.path.join(DATA_DIR, 'dashboards.template.json')
+
 BASE_URL = 'http://localhost:3000'
 BASIC_AUTH = ("admin","admin")
 LOW_ACCESS = ("LowAccess","test")
-USERS_PATH = './data/users.json'
-DASHBOARDS_PATH = './data/dashboards.json'
